eefe2032039d5488636ffdbc63a5e96a16cdb949
vitaliyvorona <voronchik84@gmail.com>
2017-02-06 20:24:22 +0200

    Check_Line

---
+++ checkio/Check_Line.py
@@ -0,0 +1,32 @@
+def check_line(line):
+    XZ_pattern = [(lambda x: 'X:Z')(x) for x in range(len(line))]
+    ZX_pattern = [(lambda x: 'Z:X')(x) for x in range(len(line))]
+
+    isCrystal = True
+    pattern_holder = []
+    counter = 0
+
+    if line[0] == 'X':
+        for i in XZ_pattern:
+            pattern_holder.extend(i.split(':'))
+        while counter < len(line):
+            if line[counter] != pattern_holder[counter]:
+                isCrystal = False
+                break
+            else:
+                counter += 1
+    if line[0] == 'Z':
+        for i in ZX_pattern:
+            pattern_holder.extend(i.split(':'))
+        while counter < len(line):
+            if line[counter] != pattern_holder[counter]:
+                isCrystal = False
+                break
+            else:
+                counter += 1
+    return isCrystal
+
+
+test2 = check_line(['X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z'])
+
+print(test2)
